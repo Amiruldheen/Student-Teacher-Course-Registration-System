cc931fdbaa86ece0cce0954d4ce3211ae227b15e
Amiruldheen <amiruldheenhn@gmail.com>
2024-09-22 00:50:56 +0530

    Initial commit for Student-Teacher Course Registration System

---
+++ course_portal/app_edu_enroll/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class Student(models.Model):
+    '''
+    To represent the Student Deatils
+    '''
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    email = models.EmailField()
+    course = models.ManyToManyField('Course', related_name='students')
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Teacher(models.Model):
+    '''
+    To represent teacher Deatails
+    '''
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    # course = models.ManyToManyField('Course', related_name='teachers')
+
+    def __str__(self) -> str:
+        return f"{self.name}-{self.id}"
+
+
+
+class Course(models.Model):
+    '''
+    To represent Course Deatails.
+    '''
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    # student = models.ManyToManyField(Student, related_name='student_courses')
+    teacher = models.ForeignKey(Teacher, related_name='teacher_courses', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+
